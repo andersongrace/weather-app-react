93260d335d79e0031138eb41016112fc39682e71
andersongrace <139086818+andersongrace@users.noreply.github.com>
2024-05-02 12:25:09 -0500

    uploading to github

---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from 'react-loader-spinner';
+
+export default function Weather(props) {
+    function handleResponse(response) {
+    alert(
+        `The weather in ${response.data.name} is 
+        ${response.data.main.temp}°F`)
+}
+let apiKey = "1ac062741764d4e19635341372dd2364";
+let apiUrl = `https://api.openweathermap.org/data/3.0/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(handleResponse);
+
+    return <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>;
+}
