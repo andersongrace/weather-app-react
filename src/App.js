865cbb4eba258c8ee860ca21e5dc4cc1d4e48ed5
andersongrace <139086818+andersongrace@users.noreply.github.com>
2024-05-18 19:48:41 -0500

    added forecast to app

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
     <div className="App">
       <header className="container">
 <Weather defaultCity={"Dallas"} />
-      </header>
-    
+     <WeatherForecastDay data={dailyForecast} />
+    </header>
     <div class="footer">
       This project was coded by Grace Anderson and is{" "} 
       <a href="https://github.com/andersongrace/weather-app-react">
         Open-sourced on Github</a> {" "} 
         and {" "} 
         <a href="https://master--imaginative-kangaroo-907531.netlify.app/">hosted on Netlify</a>
-        
+         
   </div>
   </div>
   );
